765f001c4ee3cd95e7f71b377b844183f8eeda61
Simei Yeh <simei.yeh@gmail.com>
2020-10-19 10:55:33 -0700

    Added nested components

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-// TODO
+var Cucumbers = () => {
+  return (
+    <li>Cucumbers</li>
+  )
+}
+
+var Kale = () => {
+  return (
+    <li>Kale</li>
+  )
+}
+
+
+var GroceryList = () => {
+  return (
+  <div>
+    <Cucumbers />
+    <Kale />
+  </div>
+  )
+}
+
+
+var App = () => (
+  <div>
+    <h1>Grocery List</h1>
+    <ul>
+      <GroceryList />
+    </ul>
+  </div>
+);
+
+
+
+
+ReactDOM.render(<App />, document.getElementById("app"));
